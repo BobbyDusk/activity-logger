80a101dafa7a9d537229217b49784fb0087dc91d
Robert Dusk <robert@dusk.monster>
2025-01-04 16:00:13 +0100

    finishes adding json, csv files and chapters

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import pyscreenshot as ImageGrab
 from PIL import Image, ImageDraw, ImageFont
 import subprocess
 import re
+import json
+import csv
+from dataclasses import dataclass
+from typing import Optional
+import tempfile 
 
 
 activity_logger_path = Path(Path.home(), "activity_logger")
@@ -11,6 +16,21 @@ font_path = Path(activity_logger_path, ".src/Arial.ttf")
 FPS = 2
 DURATION_DISPLAY_CARDS = 3  # in seconds
 frames_display_cards = round(FPS * DURATION_DISPLAY_CARDS)
+
+
+@dataclass(slots=True)
+class TimeBlock:
+    start_time: datetime.datetime
+    start_timestamp: datetime.timedelta
+    stop_time: Optional[datetime.datetime] = None
+    duration: Optional[datetime.timedelta] = None
+    stop_timestamp: Optional[datetime.timedelta] = None
+
+@dataclass(slots=True)
+class DayData:
+    time_blocks: list[TimeBlock]
+    total_duration: datetime.timedelta
+    date: datetime.datetime
 
 
 def get_time_string_from_date(date: datetime.datetime) -> str:
@@ -33,12 +53,47 @@ def get_date_time_string() -> str:
     return get_date_time_string_from_date(datetime.datetime.now())
 
 
+def get_date_from_folder(folder_path: Path) -> datetime.datetime:
+    date_string = folder_path.name.split("_")[0]
+    return datetime.datetime.strptime(date_string, "%Y-%m-%d")
+
+
 def check_if_date(date_string) -> bool:
     pattern = r"^\d{4}-\d{2}-\d{2}"
     return bool(re.match(pattern, date_string))
 
 
-def create_video_from_images_in_folder(folder_path: Path) -> None:
+def create_metadata_file(data: DayData) -> Path:
+    metadata_string = f""";FFMETADATA1
+title={data.date.date()}
+
+[CHAPTER]
+TIMEBASE=1/1000
+START={0}
+END={round(frames_display_cards / FPS * 1000)}
+title=summary
+    """
+
+    for index, time_block in enumerate(data.time_blocks):
+        start_time = time_block.start_timestamp.total_seconds() * 1000 + 1
+        end_time = time_block.stop_timestamp.total_seconds() * 1000
+        metadata_string += f"""
+[CHAPTER]
+TIMEBASE=1/1000
+START={round(start_time)}
+END={round(end_time)}
+title=block {index}
+        """
+
+    metadata_file = tempfile.NamedTemporaryFile(delete=False)
+    metadata_file.write(metadata_string.encode("utf-8"))
+    metadata_file.close()
+    return Path(metadata_file.name)
+
+
+def create_video_from_images_in_folder(data: DayData, folder_path: Path) -> None:
+    metadata_path = create_metadata_file(data)
+    video_path = Path(folder_path, "video.mp4")
     subprocess.run(
         [
             "ffmpeg",
@@ -48,12 +103,19 @@ def create_video_from_images_in_folder(folder_path: Path) -> None:
             "glob",
             "-i",
             f"{folder_path}/*.jpg",
+
+            "-i",
+            f"{metadata_path}",
+            "-map_metadata",
+            "1",
+
             "-vcodec",
             "libx265",
             "-y",
-            f"{folder_path}/video.mp4",
+            f"{video_path}",
         ]
     )
+
 
 
 def check_if_video_exists(folder_path: Path) -> bool:
@@ -109,72 +171,36 @@ def create_text_image(header: str, body: list[str]) -> Image:
     return image
 
 
-def get_base_card_text(
-    start_time: datetime.datetime, stop_time: datetime.datetime
-) -> list[str]:
-    return [
-        f"from {get_time_string_from_date(start_time)}",
-        f"to {get_time_string_from_date(stop_time)}",
-        f"duration: {stop_time - start_time}",
-    ]
-
-
-def save_card(image, folder_path, base_name):
-    for i in range(frames_display_cards):
-        image_path = Path(folder_path, f"{base_name}_{i}.jpg")
-        image.save(image_path)
-
-
-def process_start_time(
-    start_time: datetime.datetime, stop_time: datetime.datetime, folder_path: Path
-) -> None:
-    image = create_text_image("START", get_base_card_text(start_time, stop_time))
-    save_card(
-        image,
-        folder_path,
-        f"{get_date_time_string_from_date(start_time - datetime.timedelta(minutes=1))}_START",
-    )
-
-
-def process_stop_time(
-    start_time: datetime.datetime, stop_time: datetime.datetime, folder_path: Path
-) -> None:
-    image = create_text_image("STOP", get_base_card_text(start_time, stop_time))
-    save_card(
-        image,
-        folder_path,
-        f"{get_date_time_string_from_date(stop_time + datetime.timedelta(minutes=1))}_STOP",
-    )
-
-
 def get_files_in_folder(folder_path: Path) -> list[Path]:
     return sorted(list(folder_path.glob("*.jpg")))
 
 
-def insert_start_stop_images(folder_path: Path) -> None:
+def analyze(folder_path: Path) -> DayData:
     files = get_files_in_folder(folder_path)
     previous_date_time = None
-    time_blocks = []
+    time_blocks: list[TimeBlock] = []
 
     def add_start(time, index):
         time_blocks.append(
-            {
-                "start_time": time,
-                "start_index": index,
-                "start_timestamp": datetime.timedelta(
+            TimeBlock(
+                start_time=time,
+                start_timestamp=datetime.timedelta(
                     seconds=(
-                        (index + 2 * frames_display_cards * len(time_blocks)) / FPS
+                        (index + 2 * frames_display_cards * len(time_blocks) + 1 * frames_display_cards)
+                        / FPS
                     )
-                ),
-            }
+                )
+            )
         )
 
     def add_stop(time, index):
-        time_blocks[-1]["stop_time"] = time
-        time_blocks[-1]["stop_index"] = index
-        time_blocks[-1]["stop_timestamp"] = datetime.timedelta(
-            seconds=(((index + 1) + 2 * frames_display_cards * len(time_blocks)) / FPS)
+        time_blocks[-1].stop_time = time
+        time_blocks[-1].stop_timestamp = datetime.timedelta(
+            seconds=(
+                ((index + 1) + 2 * frames_display_cards * len(time_blocks) + 1 * frames_display_cards) / FPS
+            )
         )
+        time_blocks[-1].duration = time - time_blocks[-1].start_time
 
     for index, image_path in enumerate(files):
         date_time = datetime.datetime.strptime(image_path.stem, "%Y-%m-%d_%H:%M:%S")
@@ -187,42 +213,72 @@ def insert_start_stop_images(folder_path: Path) -> None:
             add_start(date_time, index)
         previous_date_time = date_time
 
-    summary_body = []
-    for index, time_block in enumerate(time_blocks):
-        summary_body.append(
-            f"block {index + 1}: {get_time_string_from_date(time_block.get('start_time'))} - {get_time_string_from_date(time_block.get('stop_time'))} ({time_block.get('stop_time') - time_block.get('start_time')})"
-        )
     total_duration_seconds = sum(
-        [
-            (time_block.get("stop_time") - time_block.get("start_time")).total_seconds()
-            for time_block in time_blocks
-        ]
+        [time_block.duration.total_seconds() for time_block in time_blocks]
     )
     total_duration = datetime.timedelta(seconds=total_duration_seconds)
+
+    date = get_date_from_folder(folder_path)
+
+    return DayData(
+        time_blocks=time_blocks,
+        total_duration=total_duration,
+        date=date,
+    )
+
+
+def save_card(image, folder_path, base_name):
+    for i in range(frames_display_cards):
+        image_path = Path(folder_path, f"{base_name}_{i}.jpg")
+        image.save(image_path)
+
+
+def insert_summary_card(data: DayData, folder_path: Path) -> None:
+    summary_body = []
+    for index, time_block in enumerate(data.time_blocks):
+        summary_body.append(
+            f"block {index + 1}: {get_time_string_from_date(time_block.start_time)} - {get_time_string_from_date(time_block.stop_time)} ({time_block.duration})"
+        )
     summary_body.append("—————————————————")
-    summary_body.append(f"Total time: {total_duration}")
+    summary_body.append(f"Total time: {data.total_duration}")
     image = create_text_image("Summary", summary_body)
     time_string = get_date_time_string_from_date(
-        time_blocks[0].get("start_time") - datetime.timedelta(minutes=2)
+        data.time_blocks[0].start_time - datetime.timedelta(minutes=2)
     )
     base_name = f"{time_string}_SUMMARY"
     save_card(image, folder_path, base_name)
-    # All timestaps need to be increased because of summary
-    for i in range(len(time_blocks)):
-        time_blocks[i]["start_timestamp"] = time_blocks[i][
-            "start_timestamp"
-        ] + datetime.timedelta(seconds=frames_display_cards / FPS)
-        time_blocks[i]["stop_timestamp"] = time_blocks[i][
-            "stop_timestamp"
-        ] + datetime.timedelta(seconds=frames_display_cards / FPS)
 
-    for time_block in time_blocks:
-        process_start_time(
-            time_block.get("start_time"), time_block.get("stop_time"), folder_path
-        )
-        process_stop_time(
-            time_block.get("start_time"), time_block.get("stop_time"), folder_path
-        )
+
+def get_base_card_text(time_block: TimeBlock) -> list[str]:
+    return [
+        f"from {get_time_string_from_date(time_block.start_time)}",
+        f"to {get_time_string_from_date(time_block.stop_time)}",
+        f"duration: {time_block.duration}",
+    ]
+
+
+def process_start_time(time_block: TimeBlock, folder_path: Path) -> None:
+    image = create_text_image("START", get_base_card_text(time_block))
+    save_card(
+        image,
+        folder_path,
+        f"{get_date_time_string_from_date(time_block.start_time - datetime.timedelta(minutes=1))}_START",
+    )
+
+
+def process_stop_time(time_block: TimeBlock, folder_path: Path) -> None:
+    image = create_text_image("STOP", get_base_card_text(time_block))
+    save_card(
+        image,
+        folder_path,
+        f"{get_date_time_string_from_date(time_block.stop_time + datetime.timedelta(minutes=1))}_STOP",
+    )
+
+
+def insert_start_stop_cards(data: DayData, folder_path: Path) -> None:
+    for time_block in data.time_blocks:
+        process_start_time(time_block, folder_path)
+        process_stop_time(time_block, folder_path)
 
 
 # Completely black images indicate that the computer was sleeping,
@@ -254,12 +310,42 @@ def move_video_and_remove_folder(folder_path: Path) -> None:
     # subprocess.run(["rm", "-rf", folder_path])
 
 
+def save_json_summary(data: DayData, folder_path) -> None:
+    data_dict = {
+        "blocks": [{
+           "start_time": str(time_block.start_time.time()),
+           "stop_time": str(time_block.stop_time.time()),
+           "duration": str(time_block.duration),
+           "start_timestamp": str(time_block.start_timestamp),
+           "stop_timestamp": str(time_block.stop_timestamp),
+        } for time_block in data.time_blocks],
+        "total_duration": str(data.total_duration),
+        "date": str(data.date.date())
+    }
+    with open(Path(activity_logger_path, f"{folder_path.name}.json"), "w") as f:
+        json.dump(data_dict, f, indent=4)
+
+
+def save_csv_summary(data: DayData, folder_path) -> None:
+    with open(Path(activity_logger_path, f"{folder_path.name}.csv"), "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["date", "start", "end", "duration"])
+        for block in data.time_blocks:
+            writer.writerow(
+                [data.date.date(), block.start_time.time(), block.stop_time.time(), block.duration]
+            )
+
+
 def process_folder(folder_path: Path) -> None:
     if folder_path:
         remove_black_images(folder_path)
-        insert_start_stop_images(folder_path)
-        create_video_from_images_in_folder(folder_path)
+        data = analyze(folder_path)
+        insert_summary_card(data, folder_path)
+        insert_start_stop_cards(data, folder_path)
+        create_video_from_images_in_folder(data, folder_path)
         move_video_and_remove_folder(folder_path)
+        save_json_summary(data, folder_path)
+        save_csv_summary(data, folder_path)
 
 
 def write_date_time_on_image(image) -> Image:
