a1bf0430ec83f237421700c475c1da045c0608e7
Bobby Dusk <bobby@dusk.monster>
2025-02-06 06:46:55 +0100

    fixes bug with hardcoded path to home directory in service file

--- setup.sh
+++ setup.sh
@@ -3,9 +3,11 @@
 DIR=$HOME/activity_logger
 
 # install ffmpeg
+echo "Installing ffmpeg"
 sudo apt install ffmpeg -y
 
 # copy source files
+echo "Copying source files"
 mkdir $DIR/.src -p
 cp ./main.py $DIR/.src/main.py
 cp ./run.sh $DIR/.src/run.sh
@@ -14,14 +16,18 @@ cp ./pyproject.toml $DIR/.src/pyproject.toml
 cp ./uv.lock $DIR/.src/uv.lock
 
 # copy the systemd files
+echo "Copying systemd files"
 mkdir $HOME/.config/systemd/user -p
 cp ./activity_logger.service $HOME/.config/systemd/user/activity_logger.service
 cp ./activity_logger.timer $HOME/.config/systemd/user/activity_logger.timer
 
 # install dependencies
+echo "Installing dependencies"
 cd $DIR/.src
 uv sync
 
 # enable and start the service
+echo "Enabling and starting the service"
 systemctl --user daemon-reload
 systemctl --user enable activity_logger.timer
+systemctl --user start activity_logger.timer
